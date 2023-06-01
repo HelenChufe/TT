f665a4f0a89bc02d7606d8e01207816827d5e25c
Osvaldo Jusaino <131230066+HelenChufe@users.noreply.github.com>
2023-06-01 15:07:42 -0700

    Creacion de la Main Page.

--- tartips/views.py
+++ tartips/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+    '''
+    Args: None
+    Process:None
+    Return:Devuelve el render de la pagina principal.
+    '''
+    return render(request,'main.html')
